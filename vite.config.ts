0c055bae7d18e102b8a6c3cfe5a507e396689f15
zvonimir.zunic <zvonimir.zunic@power-up.green>
2025-01-14 17:39:05 +0100

    vite configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +11,11 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  build: {
+    outDir: 'dist', 
+  },
+  base: '/', 
+});
